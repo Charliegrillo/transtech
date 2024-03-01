dadc6e21489edc94343a52192580a06d387568ec
Charlie <charlie@hexmatrix.net>
2024-03-01 13:35:33 -0400

    fix:Fixes clean

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 
 'use client'
 import { logout } from '@/lib/actions';
-import { Button } from '@/components/ui/button';
 import { useTenantContext } from '@/context/TenantContext';
 
 const About = async () => {
@@ -13,9 +12,9 @@ const About = async () => {
 			className='h-screen w-screen flex flex-col justify-center items-center gap-10'
 		>
 			<p className='text-white'>{tenant}</p>
-			<Button type='submit' className='w-40' variant='secondary'>
+			<button type='submit' className='w-40'>
 				logout
-			</Button>
+			</button>
 		</form>
 	);
 };
